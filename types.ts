e88d535541e9b0343242ce29a35daa863e51a685
Sean aye <hello@seanaye.ca>
2021-06-22 13:57:46 -0700

    fix types

--- types.ts
+++ types.ts
@@ -3,6 +3,4 @@ import { ServerRequest } from 'https://deno.land/std@0.99.0/http/server.ts'
 /**
  * Request type with only required properties
  */
-export type Request =
-  | Pick<ServerRequest, "respond" | "body" | "method" | "headers">
-  | NativeRequest;
+export type Request = Pick<ServerRequest, 'respond' | 'body' | 'method' | 'headers'>
